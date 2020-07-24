f39c5facbb1fdd848d4e2a958c9aa1fbcef61f0d
Reyna-5970 <55515949+Reyna-5970@users.noreply.github.com>
2020-07-24 16:47:05 -0700

    Filled in sensor details func

--- Code/C/IPtest/IPtest.ino
+++ Code/C/IPtest/IPtest.ino
@@ -15,11 +15,32 @@
 Adafruit_FXAS21002C gyro = Adafruit_FXAS21002C(0x0021002C);
 
 void displaySensorDetails(void) {
-  // nothing yet
+  sensor_t sensor;
+  gyro.getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.print("Sensor:       ");
+  Serial.println(sensor.name);
+  Serial.print("Driver Ver:   ");
+  Serial.println(sensor.version);
+  Serial.print("Unique ID:    0x");
+  Serial.println(sensor.sensor_id, HEX);
+  Serial.print("Max Value:    ");
+  Serial.print(sensor.max_value);
+  Serial.println(" rad/s");
+  Serial.print("Min Value:    ");
+  Serial.print(sensor.min_value);
+  Serial.println(" rad/s");
+  Serial.print("Resolution:   ");
+  Serial.print(sensor.resolution);
+  Serial.println(" rad/s");
+  Serial.println("------------------------------------");
+  Serial.println("");
+  delay(500);
 }
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  displaySensorDetails();
 
 }
 
