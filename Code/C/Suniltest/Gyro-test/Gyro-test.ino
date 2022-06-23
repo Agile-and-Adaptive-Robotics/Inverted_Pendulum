3c202840dda99cd563abe1ac70b886ebbe473820
sunilwilliams <107816343+sunilwilliams4@users.noreply.github.com>
2022-06-22 21:03:36 -0700

    Combined most aspects into Gyto-test for testing.

--- Code/C/Suniltest/Gyro-test/Gyro-test.ino
+++ Code/C/Suniltest/Gyro-test/Gyro-test.ino
@@ -1,53 +1,139 @@
 
+// ------------------ // Gyroscope stuff
+
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_L3GD20_U.h>
 
-
 Adafruit_L3GD20_Unified gyro = Adafruit_L3GD20_Unified(20);
 
+// ------------------ // Encoder Stuff
 
-void setup() {
-  Serial.begin(9600);
+#include <Arduino.h>
+#include <RotaryEncoder.h>
 
-  gyro.enableAutoRange(true);
 
-  if (!gyro.begin()) {
-    Serial.println("gyro no working");
-  }
+RotaryEncoder encoder(11, 12, RotaryEncoder::LatchMode::TWO03);
 
-  //sensor_t sensor;
-  //gyro.getSensor(&sensor);
-  //Serial.println("------------------------------------");
-  //Serial.print  ("Sensor:       "); Serial.println(sensor.name);
-  //Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
-  //Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
-  //Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" rad/s");
-  //Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" rad/s");
-  //Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" rad/s");  
-  //Serial.println("------------------------------------");
-  //Serial.println("");
+int lastPosition = 0;
+int currentPosition = 0;
 
-}
+
+// ------------------ //
+
 
 int checksPerSecond = 100;
 
 float angle = PI / 2;
 float angleVelocity = 0;
+const float targetAngle = PI / 2; // angle straight up (90 degrees)
+
+
+// ------------------ // valve open amounts -- floats from 0 to 1
+
+// 1 = fully open, 0 = closed
+float leftValveOpen = 0;
+float rightValveOpen = 0;
+
+const float EQUALIBRIUM_OPEN = .5;
+
+const int LEFT_VALVE_PIN = 3;
+const int RIGHT_VALVE_PIN = 5;
+
+
+// ------------------------------------------------- //
+
+
+void setup() {
+  Serial.begin(9600);
+  while(!Serial);
+
+  Serial.println("starting");
+
+  pinMode(LEFT_VALVE_PIN, OUTPUT);
+  pinMode(RIGHT_VALVE_PIN, OUTPUT);
+
+  pinMode(11, INPUT);
+  pinMode(12, INPUT);
+
+  gyro.enableAutoRange(true);
+
+  if (!gyro.begin()) {
+    Serial.println("gyro not working");
+  }
+
+  
+}
+
+
+// ------------------------------------------------- //
+
 
 void loop() {
 
+  // ------------------ // calculate angle
+
   sensors_event_t event;
   gyro.getEvent(&event);
-
   
-
   angleVelocity = event.gyro.z;
 
   angle += (angleVelocity - .14) / checksPerSecond;
 
   Serial.println(angle * (180 / PI));
-  //Serial.println(angleVelocity);
+
+  // ------------------ // calculate pwm to send to valves
+
+  /*
+   to move to the right, leftValveOpen decreases (left valve closes, left BPA inflates) and opposite for right valve
+   opposite is done to move to the left
+  */
+  
+  float correctionAmount = (targetAngle - angle) / 2;
+
+  angle += correctionAmount / 10; // changing angle just for simulation, in real life this would change due valves closing/opening
+
+
+  leftValveOpen = EQUALIBRIUM_OPEN - correctionAmount, 0, 1; // close more (inflate) to move to the right (positive correctionAmount)
+  rightValveOpen = EQUALIBRIUM_OPEN + correctionAmount, 0, 1; // open more (deflate) to move to right (with positive correctionAmount)
+
+  // ------ // make sure pressure isn't too high
+
+  float leftPressure = 50; // SET THIS LATER, 50 is just a placeholder
+  float rightPressure = 50;
+
+  float maxPressure = 90;
+
+  if (leftPressure > maxPressure) {
+    leftValveOpen += .1;
+  }
+  if (rightPressure > maxPressure) {
+    rightValveOpen += .1;
+  }
+
+  // ------ // make sure pressure isn't too high
+
+  if (leftValveOpen < 0) leftValveOpen = 0;
+  if (leftValveOpen > 1) leftValveOpen = 1;
+  if (rightValveOpen < 0) rightValveOpen = 0;
+  if (rightValveOpen > 1) rightValveOpen = 1;
+  
+  // ------------------// change pwm of valves
+  
+  digitalWrite(LEFT_VALVE_PIN, leftValveOpen * 255);
+  digitalWrite(RIGHT_VALVE_PIN, rightValveOpen * 255);
+
+  // ------------------// encoder testing
+
+  encoder.tick();
+
+  int currentPosition = encoder.getPosition();
+
+  if (currentPosition != lastPosition) {
+    //Serial.println((int)(encoder.getDirection()));
+    lastPosition = currentPosition;
+  }
+
 
   delay(1000 / checksPerSecond);
 }
