4f166eb876919d98e7d6ec50bb46beafb43f835e
sunilwilliams4 <107816343+sunilwilliams4@users.noreply.github.com>
2022-06-22 17:03:21 -0700

    Made gyro absolute angle calculation code.

--- Code/C/Suniltest/Gyro-test/Gyro-test.ino
+++ Code/C/Suniltest/Gyro-test/Gyro-test.ino
@@ -1,9 +1,53 @@
+
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_L3GD20_U.h>
+
+
+Adafruit_L3GD20_Unified gyro = Adafruit_L3GD20_Unified(20);
+
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  gyro.enableAutoRange(true);
+
+  if (!gyro.begin()) {
+    Serial.println("gyro no working");
+  }
+
+  //sensor_t sensor;
+  //gyro.getSensor(&sensor);
+  //Serial.println("------------------------------------");
+  //Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  //Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  //Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  //Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" rad/s");
+  //Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" rad/s");
+  //Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" rad/s");  
+  //Serial.println("------------------------------------");
+  //Serial.println("");
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+int checksPerSecond = 100;
 
+float angle = PI / 2;
+float angleVelocity = 0;
+
+void loop() {
+
+  sensors_event_t event;
+  gyro.getEvent(&event);
+
+  
+
+  angleVelocity = event.gyro.z;
+
+  angle += (angleVelocity - .14) / checksPerSecond;
+
+  Serial.println(angle * (180 / PI));
+  //Serial.println(angleVelocity);
+
+  delay(1000 / checksPerSecond);
 }
