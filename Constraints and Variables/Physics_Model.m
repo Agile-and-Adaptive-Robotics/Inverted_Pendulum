d0cbd95d9dd2b6c39e2d7677969999a2717c8dab
Pablo-Riffel <pabloriffel@icloud.com>
2024-07-19 15:53:14 -0700

    Created MATLAB model and refreshed spreadsheet model

---
+++ Constraints and Variables/Physics_Model.m
@@ -0,0 +1,6 @@
+%variable inputs
+h_object        = 1.3;      % height of object, in meters
+m_object        = 80;       % mass of object, in kilograms
+vmax_platform   = .03528;   % maximum speed of platform, in radians/second
+dt              = .125;     % time for platform to go to one side and return to center, in seconds
+
